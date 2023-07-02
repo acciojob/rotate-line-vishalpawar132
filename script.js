e5a84682072d7a1ad13f2a1d44aab867e9761f03
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-02 10:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const line = document.getElementById("line");
+let rotation = 0;
 
+setInterval (() =>{
+	rotation += 5;
+	line.style.transform = 'translate(-50%, -50%) rotate (${rotation})deg)';
+}, 50);
